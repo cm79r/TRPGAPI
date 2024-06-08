52c6a197d297e67827c2adea136c00d631516529
cm79r <33876544+cm79r@users.noreply.github.com>
2024-06-08 15:13:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ const html = `
   </head>
   <body>
     <section>
-      <a href="https://render.com/docs/deploy-node-fastify-app">Hello from Render using Fastify!</a>
+      <a href="https://render.com/docs/deploy-node-fastify-app">Place Holder Text!</a>
     </section>
   </body>
 </html>
