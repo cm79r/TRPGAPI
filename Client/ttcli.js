6e296b6519d59d5fd157dd49079060c09004135d
cm79r <33876544+cm79r@users.noreply.github.com>
2024-06-09 13:03:23 -0400

    000001

---
+++ Client/ttcli.js
@@ -0,0 +1,44 @@
+/*
+Table Top Client Code
+This is just a reference doc in the form of some snipets and not an actual client.
+*/
+
+class TTCLI {
+    constructor(id, command){
+        // construct JSON to send to API.
+        //Identifier assigned by API
+        this._id = id || 0;
+
+        //command as a lowercase string, X Y pairs for when the action should happen.
+        this._commmand = command || ["string", [0,0]];
+
+        // this may not be needed either.
+        this._commandSent = false;
+
+        //A few methods down here
+        this.sendCommand = function(){           
+            // Creating a XHR object
+            var xhr = new XMLHttpRequest();
+            var url = endpointhere;
+       
+            // open a connection
+            xhr.open("POST", url, true);
+ 
+            // Set the request header i.e. which type of content you are sending
+            xhr.setRequestHeader("Content-Type", "application/json");
+ 
+            // Create a state change callback
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                     console.log(xhr.responseText);
+                }
+            };
+ 
+            // Converting JSON data to string
+            var data = JSON.stringify(this._commmand);
+ 
+            // Sending data with the request
+            xhr.send(data);
+        }
+    }
+};
